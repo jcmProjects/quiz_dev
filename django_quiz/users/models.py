687812d06b4e83da17809679033c5531788d0a03
João Moreira <jcmoreira@ua.pt>
2019-03-16 01:17:02 +0000

    changed the display name of the objects on admin page

--- django_quiz/users/models.py
+++ django_quiz/users/models.py
@@ -9,18 +9,18 @@ class Profile(models.Model):
     image = models.ImageField(default='default_profile_pic.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user}'
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        img = Image.open(self.image)
 
         # Resize Image
         if img.height>300 or img.width>300:
             output_size = (300, 300)
             img.thumbnail(output_size)
-            img.save(self.image.path)
+            img.save(self.image)
 
 
 
@@ -38,4 +38,4 @@ class ProfileCourse(models.Model):
     course = models.ForeignKey('Course', on_delete=models.DO_NOTHING)
 
     def __str__(self):
-        return '{}'.format(self.course)
+        return f'Course: {self.course}, User: {self.profile}'
